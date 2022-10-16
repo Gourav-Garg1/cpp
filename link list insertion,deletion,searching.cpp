55e4e85eee27733dc2f1d8fa2f3ae84eb2229fb4
Gourav-Garg1 <111060711+Gourav-Garg1@users.noreply.github.com>
2022-10-16 23:55:47 +0530

    Add files via upload

---
+++ link list insertion,deletion,searching.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int val){
+        data=val;
+        next=NULL;
+    }
+
+};
+
+void insertathead(node* &head, int val){
+    node* n= new node(val);
+    n->next=head;
+    head=n;
+}
+
+void insertattail(node* &head, int val){
+    node* n= new node(val);
+    if(head==NULL){
+        head=n;
+        return;
+    }
+
+
+    node* temp = head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=n;
+}
+
+    void display(node* head){
+        node* temp=head;
+        while(temp!=NULL){
+            cout<<temp->data<<"->";
+        
+        temp=temp->next;
+    }
+    cout<<"null";
+    }
+
+    bool search(node* head,int key){
+        node* temp=head;
+        while(temp!=NULL){
+            if(temp->data==key){
+                return true;
+            }
+            temp=temp->next;
+        }
+        return false;
+    }
+    void deleteathead(node* &head){
+        node* todelete=head;
+        head=head->next;
+    }
+    void deletion(node* &head,int val){
+        if(head==NULL){
+            return;
+        }
+        if(head->next==NULL){
+            deleteathead(head);
+            return;
+        }
+        node* temp=head;
+        while(temp->next->data!=val){
+            temp=temp->next;
+
+        }
+        node* todelete=temp->next;
+        temp->next=temp->next->next;
+        delete todelete;
+    }
+int main() {
+    cout<<"Hello World!"<<endl;
+
+    node* head=NULL;
+    insertattail(head,1);
+    insertattail(head,2);
+    insertattail(head,3); 
+    insertathead(head,4);
+    display(head);
+    cout<<search(head,4)<<endl;
+    deletion(head,3);
+    deleteathead(head);
+    display(head);
+}
